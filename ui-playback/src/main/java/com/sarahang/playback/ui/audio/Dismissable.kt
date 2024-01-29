79765e85b9ff010b9e5dc06e13a219df2edf36c7
vipulkumar <vipulyaara@gmail.com>
2024-01-29 11:52:35 +0530

    Update deps

--- ui-playback/src/main/java/com/sarahang/playback/ui/audio/Dismissable.kt
+++ ui-playback/src/main/java/com/sarahang/playback/ui/audio/Dismissable.kt
@@ -1,31 +1,22 @@
-/*
- * Copyright (C) 2021, Alashov Berkeli
- * All rights reserved.
- */
 package com.sarahang.playback.ui.audio
 
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.DismissValue
-import androidx.compose.material3.SwipeToDismiss
-import androidx.compose.material3.rememberDismissState
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Dismissable(
-    onDismiss: () -> Unit,
-    directions: Set<DismissDirection> = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
-    content: @Composable () -> Unit
-) {
-    val dismissState = rememberDismissState(confirmValueChange = {
-        if (it != DismissValue.Default) {
+fun Dismissable(onDismiss: () -> Unit, content: @Composable () -> Unit) {
+    val dismissState = rememberSwipeToDismissBoxState(confirmValueChange = {
+        if (it != SwipeToDismissBoxValue.Settled) {
             onDismiss.invoke()
         }
         true
     })
-    SwipeToDismiss(
+
+    SwipeToDismissBox(
         state = dismissState,
-        directions = directions,
-        background = {},
-        dismissContent = { content() }
+        backgroundContent = {},
+        content = { content() }
     )
 }
